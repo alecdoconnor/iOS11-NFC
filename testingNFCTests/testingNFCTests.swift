55ba9bfd113a92faf3f8e4aa99a37a35b5ad0c32
Alec O’Connor <alec.oconnor@universalorlando.com>
2017-06-06 13:08:07 -0400

    Initial Commit

---
+++ testingNFCTests/testingNFCTests.swift
@@ -0,0 +1,36 @@
+//
+//  testingNFCTests.swift
+//  testingNFCTests
+//
+//  Created by Alec O'Connor on 6/6/17.
+//  Copyright © 2017 Universal Orlando. All rights reserved.
+//
+
+import XCTest
+@testable import testingNFC
+
+class testingNFCTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
